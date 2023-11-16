e8a69dc2135bbe6bb0db2172a928385b4b8e50f6
2joopiter <veryberrylike3@gmail.com>
2023-11-16 12:49:02 +0900

    동적으로 생성된 이미지DOM을 인수로 받아 소스 이미지 로딩유무 체크 함수

--- js/main.js
+++ js/main.js
@@ -5,53 +5,22 @@ const imgNum = 200;
 
 let count = 0;
 
-createImgs(frame, imgNum);
-const imgs = frame.querySelectorAll('img');
+const imgs = createImgs(frame, imgNum);
+imgLoadedCheck(imgs);
 
 //각 동적생성 이미지 요소를 반복처리
-imgs.forEach((img) => {
-	//만약 특정 이미지의 소스를 불러오지 못해 엑박이 뜰 시 대체이미지 출력
-	img.addEventListener('error', (e) => {
-		console.log('해당 이미지 로딩 실패', e);
-		e.currentTarget.setAttribute('src', 'logo.png');
-	});
-	//특정 이미지 렌더링 성공시 count값 증가처리
-	img.addEventListener('load', () => {
-		//카운트값을 다시 백분율로 변환해서 로딩화면에 출력
-		countEl.innerText = parseInt((count / imgNum) * 100);
-		count++;
-		//카운트 갯수와 전체 이미지 갯수가 동일해지면(모든 이미지 소스가 렌더링 완료시) 마스크를 제거
-		if (count === imgNum) {
-			mask.remove();
-		}
-	});
-});
 
 frame.addEventListener('mousemove', (e) => {
 	const { pageX } = e; // = const pageX = e.pageX
 	console.log('x축', pageX);
 
-	// 백분율 공식 : 현재수치값 / 전체수치값 * 100
 	const percent = parseInt((pageX / window.innerWidth) * imgNum);
-	//parseInt('숫자'): 인수로 전달된 값에 소수점 아래는 버리고 정수를 변환
-	//parseFloat('숫자'): 인수로 전달된 값의 소수점까지 포함한 실수로 반환
 
 	imgs.forEach((img) => (img.style.visibility = 'hidden'));
 	imgs[percent].style.visibility = 'visible';
 });
-//이미지 넘버와 이미지 개수가 매칭되면서 해당 축의 넘버에 따라 그 넘버에 해당되는 이미지가 뜨는 시스템을 구축한것임
 
-/* 
-  전체 작업 흐름
-  1. 동적으로 200개의 img DOM 생성
-  2. 마우스 무브시 포인터의 가로 좌표값을 200분율로 변환
-  3. 200분율 중 현재 마우스 포인터 위치 순번에 따른 이미지만 보임처리
-  4. 이미지돔이 생성되고 이미지 소스가 로딩될때마다 로딩 순번을 100분율처리
-  5. 이미지 생성시 해당 이미지에 에러 발생시 대체이미지로 변경 처리
-  6. 이미지소스가 모두 로딩되기 전까지는 마스크화면으로 가려주면서 로딩상황 백분율 출력
-  7. 모든 이미지 소스 로딩 완료시 마스크화면 제거
-*/
-
+//동적 이미지 생성함수 분리 (돔 생성하자마자 바로 리턴해서 활용 가능하도록 처리)
 function createImgs(frame, imgNum, imgName = 'pic') {
 	let tags = '';
 
@@ -59,4 +28,22 @@ function createImgs(frame, imgNum, imgName = 'pic') {
 		tags += `<img src='img/${imgName}${i}.jpg' />`;
 	}
 	frame.innerHTML = tags;
+	return frame.querySelectorAll('img');
+}
+
+// 이미지 소스 로딩유무 체크함수
+function imgLoadedCheck(imgs) {
+	imgs.forEach((img) => {
+		img.addEventListener('error', (e) => {
+			console.log('해당 이미지 로딩 실패', e);
+			e.currentTarget.setAttribute('src', 'logo.png');
+		});
+		img.addEventListener('load', () => {
+			countEl.innerText = parseInt((count / imgNum) * 100);
+			count++;
+			if (count === imgNum) {
+				mask.remove();
+			}
+		});
+	});
 }
